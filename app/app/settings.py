35d126ed08471e3622641ba1ff9fc9b119b122d6
garfdev42 <santiagoganjah420@gmail.com>
2024-03-01 17:16:58 -0500

    Segundo Commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       #'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE':'django.db.backends.postgresql_psycopg2',
+       'NAME': 'app',
+       'USER': 'postgres',
+       'PASSWORD': 'unicesmag',
+       'HOST': 'localhost',
+       'PORT': '5432',
+
     }
 }
 
