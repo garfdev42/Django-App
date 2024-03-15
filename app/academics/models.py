eeb3816369015e6a75f0b4757040212397ebed1a
garfdev42 <santiagoganjah420@gmail.com>
2024-03-15 17:18:44 -0500

    tercer commit

--- app/academics/models.py
+++ app/academics/models.py
@@ -3,12 +3,23 @@ from django.db import models
 import datetime
 
 #Va indentado principalemte tabulacion 
+#model.DateTimeField(auto_now=True, null=False) =>
+#model.DateTimeField() 
+
+
 class User(models.Model):
-    firstname = models.CharField(max_length = 20)
-    lastname = models.CharField(max_length=20)
-    age = models.IntegerField()
-    ident_number = models.CharField(max_length=12, blank=True)
+    email = models.EmailField(null = True, blank = True)
+    password = models.CharField(null = True, blank = True)
+    status = models.BooleanField(null = True, blank = True, default = True)
     created_at = models.DateTimeField(default=datetime.datetime.now())
     updated_at = models.DateTimeField(default=datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
 
+class Person(models.Model):
+     firstname = models.CharField(max_length = 20)
+     lastname = models.CharField(max_length=20)
+     age = models.IntegerField()
+     ident_number = models.CharField(max_length=12, blank=True)
+     created_at = models.DateTimeField(default=datetime.datetime.now())
+     updated_at = models.DateTimeField(default=datetime.datetime.now())
+     deleted_at = models.DateTimeField(null = True, blank = True)
