b077c8a980bc5662ff6214501fc7b541aa64998b
garfdev42 <santiagoganjah420@gmail.com>
2024-03-20 09:33:21 -0500

    cuarto commit

--- app/academics/models.py
+++ app/academics/models.py
@@ -1,25 +1,69 @@
-from django.db import models
-# Create your models here.
 import datetime
+from django.db import models
 
-#Va indentado principalemte tabulacion 
-#model.DateTimeField(auto_now=True, null=False) =>
-#model.DateTimeField() 
-
-
+# Create your models here.
+#model.DateTimeField(auto_now = true, null = false ) => update_at
+#model.DateTimeField(auto_now_add = true, null = false ) => Defaul now()
+#usamos models.Model para usar los timpos de campos ejemplos variables
 class User(models.Model):
+    #como puedo factorizar los datos en una sola clase 
     email = models.EmailField(null = True, blank = True)
     password = models.CharField(null = True, blank = True)
     status = models.BooleanField(null = True, blank = True, default = True)
-    created_at = models.DateTimeField(default=datetime.datetime.now())
-    updated_at = models.DateTimeField(default=datetime.datetime.now())
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
 
+    #Convetir a acdena para que los datos se visualicen 
+    def __str__(self):
+        return self.email
+
+
 class Person(models.Model):
-     firstname = models.CharField(max_length = 20)
-     lastname = models.CharField(max_length=20)
-     age = models.IntegerField()
-     ident_number = models.CharField(max_length=12, blank=True)
-     created_at = models.DateTimeField(default=datetime.datetime.now())
-     updated_at = models.DateTimeField(default=datetime.datetime.now())
-     deleted_at = models.DateTimeField(null = True, blank = True)
+    firstname = models.CharField(max_length = 20) 
+    lastname = models.CharField(max_length = 20)
+    age = models.IntegerField()
+    ident_number = models.CharField(max_length = 12, blank = True) # blank = true que acaepte valores null
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class Students(models.Model):
+    code = models.CharField(max_length = 20)
+    status = models.BooleanField(null = True, blank = True, default = True)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class identification_types(models.Model):
+    name = models.CharField(max_length = 50)
+    abrev = models.CharField(max_length = 10)
+    descrip =  models.CharField(max_length = 100)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class cities(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)
+    descrip =  models.CharField(max_length = 10)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class departments(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)
+    descrip =  models.CharField(max_length = 10)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class cpuntries(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)
+    descrip =  models.CharField(max_length = 10)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
